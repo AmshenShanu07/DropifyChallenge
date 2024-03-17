e229edef2a7947e9c9951e5b4c741dda9b4e17e1
Amshen Yesudas <amshen.shanu07.ya@gmail.com>
2024-03-17 22:44:31 +0530

    feat: added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience'
 import Home from './Components/Home'
+import { Suspense } from 'react'
+import { Loader } from '@react-three/drei'
 
 const App = () => {
   return (
-    <>
+    <Suspense fallback={false}>
       <Canvas shadows camera={{ position: [0, 0, 3], fov: 30 }} >
         <ambientLight intensity={2} position={[0,2,0]} />
         <Experience/>
       </Canvas>
+      <Loader/>
       <Home/>
-    </>
+    </Suspense>
   )
 }
 
